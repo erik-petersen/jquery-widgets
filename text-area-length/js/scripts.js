5d1d369ac6fbd107763b3187d4b2de7ecb67d685
Erik Petersen <contacterikpetersen@gmail.com>
2016-05-31 22:47:52 -0400

    Added css to program

--- text-area-length/js/scripts.js
+++ text-area-length/js/scripts.js
@@ -1,16 +1,16 @@
-$( "textarea[maxsize]" ).keypress(function(e) {
+$( "textarea[maxsize]" ).keydown(function(e) {
   var maxLength = $(this).attr( "maxsize" );
-  var charRemaining = 24 - $(this).val().length;
+  var charRemaining = maxLength - $(this).val().length - 1;
 
   if( this.value.length > maxLength - 1 ) {
     $( ".characters-remaining" ).text( "Too Verbose! Brevity is the key here!!!");
-    $(this).val($(this).val().substring(0, maxLength - 1));
+    $(this).val( $(this).val().substring(0, maxLength - 1) );
   }
 
   else {
     $( ".characters-remaining" ).text( charRemaining + " characters remaining.");
     if( e.keyCode === 46 || e.keyCode === 8 ) {
-      charRemaining = $(this).val().length;
+      charRemaining = maxLength - $(this).val().length + 1;
       $( ".characters-remaining" ).text( charRemaining + " characters remaining." );
     }
   }
